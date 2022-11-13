d516ad7c6c98c093302bfba8e2270c52a0425c9c
gabizz <zzgabriel@yahoo.com>
2022-11-13 21:14:34 +0200

    --- Students Crud ---

---
+++ ManageAccommodation/Controllers/StudentController.cs
@@ -0,0 +1,121 @@
+﻿using ManageAccommodation.Models;
+using ManageAccommodation.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageAccommodation.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private Repository.StudentRepository _repository;
+
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            _repository = new Repository.StudentRepository(dbContext);
+        }
+
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            var students = _repository.GetAllStudents();
+            return View("Index", students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(Guid id)
+        {
+            var model = _repository.GetStudentById(id);
+            return View("StudentDetails", model);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View("CreateStudent");
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Models.StudentModel model = new Models.StudentModel();
+
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+
+                if (task.Result)
+                {
+                    _repository.InsertStudent(model);
+                }
+                return View("CreateStudent");
+            }
+            catch
+            {
+                return View("CreateStudent");
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var model = _repository.GetStudentById(id);
+            return View("EditStudent", model);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, IFormCollection collection)
+        {
+            try
+            {
+                var model = new StudentModel();
+
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+
+                if (task.Result)
+                {
+                    _repository.UpdateStudent(model);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Index", id);
+                }
+            }
+            catch
+            {
+                return RedirectToAction("Index", id);
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            var model = _repository.GetStudentById(id);
+            return View("DeleteStudent", model);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id, IFormCollection collection)
+        {
+            try
+            {
+                var model = _repository.GetStudentById(id);
+                _repository.DeleteStudent(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("DeleteStudent", id);
+            }
+        }
+    }
+}
---
+++ ManageAccommodation/Repository/StudentRepository.cs
@@ -0,0 +1,104 @@
+﻿using ManageAccommodation.Models;
+using ManageAccommodation.Models.DBObjects;
+
+namespace ManageAccommodation.Repository
+{
+    public class StudentRepository
+    {
+        private ApplicationDbContext dbContext;
+
+        public StudentRepository()
+        {
+            this.dbContext = new ApplicationDbContext();
+        }
+
+        public StudentRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; 
+        }
+
+        private StudentModel MapDbObjectToModel(Student dbStudent)
+        {
+            StudentModel model = new StudentModel();
+
+            if(dbStudent != null){
+                model.Idstudent = dbStudent.Idstudent;
+                model.StudentName = dbStudent.StudentName;
+                model.Mobile = dbStudent.Mobile;
+                model.Idroom = dbStudent.Idroom;
+                model.PaymStatus = dbStudent.PaymStatus;
+                model.Debt = dbStudent.Debt;
+                model.Email = dbStudent.Email;
+            }
+            return model;
+        }
+
+        private Student MapModelToDbObject(StudentModel model)
+        {
+            Student student = new Student();
+
+            if(model != null)
+            {
+                student.StudentName = model.StudentName;
+                student.Mobile = model.Mobile;  
+                student.Idroom = model.Idroom;
+                student.PaymStatus = model.PaymStatus;
+                student.Debt = model.Debt;
+                student.Idstudent = model.Idstudent;
+                student.Email = model.Email;
+            }
+            return student;
+        }
+
+        public List<StudentModel> GetAllStudents()
+        {
+            List<StudentModel> studentList = new List<StudentModel>();
+
+            foreach(Student dbStudent in dbContext.Students)
+            {
+                studentList.Add(MapDbObjectToModel(dbStudent));
+            }
+            return studentList;
+        }
+
+        public StudentModel GetStudentById(Guid id)
+        {
+            return MapDbObjectToModel(dbContext.Students.FirstOrDefault(x => x.Idstudent == id));
+        }
+
+        public void InsertStudent(StudentModel studentModel)
+        {
+            studentModel.Idstudent = Guid.NewGuid();
+
+            dbContext.Students.Add(MapModelToDbObject(studentModel));
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateStudent(StudentModel studentModel)
+        {
+            Student existingStudent = dbContext.Students.FirstOrDefault(x => x.Idstudent == studentModel.Idstudent);
+
+            if(existingStudent != null)
+            {
+                existingStudent.Idstudent = studentModel.Idstudent;
+                existingStudent.StudentName = studentModel.StudentName;
+                existingStudent.Mobile = studentModel.Mobile;
+                existingStudent.Email = studentModel.Email;
+                existingStudent.Idroom = studentModel.Idroom;
+                existingStudent.Debt = studentModel.Debt;
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void DeleteStudent(StudentModel studentModel)
+        {
+            Student existingStudent = dbContext.Students.FirstOrDefault(x => x.Idstudent == studentModel.Idstudent);
+
+            if(existingStudent != null)
+            {
+                dbContext.Students.Remove(existingStudent);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
